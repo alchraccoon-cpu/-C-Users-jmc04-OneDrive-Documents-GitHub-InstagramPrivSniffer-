1b0e677a4fa3c75a319d9dc9038ce391d3634ff1
alchraccoon-cpu <alch.raccoon@gmail.com>
2026-05-09 02:05:34 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Copyright (c) 2025 obitouka
+See the file 'LICENSE' for copying permission
+"""
+
+from core.accountDataFetcher import fetch_data 
+from core.mediaDownloader import download_media
+from lib.banner import printBanner
+from utils.parser import getArguments
+
+args = getArguments()
+
+if args.name:
+    printBanner()
+    fetch_data(args.name, debug=args.debug)
+elif args.dload:
+    printBanner()
+    download_media(args.dload, debug=args.debug)
